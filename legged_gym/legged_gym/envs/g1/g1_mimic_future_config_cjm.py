1fa365881987ca9df7444f268ed10d391d2a4f07
cjm <cuijiaming@galbot.com>
2026-02-25 21:25:51 +0800

    Exp-1 稳定优先 在基础版上加：dof_torque_limits, dof_acc 目的：抑制大力矩和高频抖动，提升可部署性

--- legged_gym/legged_gym/envs/g1/g1_mimic_future_config_cjm.py
+++ legged_gym/legged_gym/envs/g1/g1_mimic_future_config_cjm.py
@@ -204,9 +204,9 @@ class G1MimicStuFutureCJMCfg(G1MimicPrivCfg):
             # feet_contact_forces = -5e-4      
             # feet_stumble = -1.25
             dof_pos_limits = -5.0
-            # dof_torque_limits = -1.0
+            dof_torque_limits = -1.0
             dof_vel = -1e-4
-            # dof_acc = -5e-8
+            dof_acc = -5e-8
             # action_rate = -0.05
             action_rate = -0.02
             # feet_air_time = 5.0
